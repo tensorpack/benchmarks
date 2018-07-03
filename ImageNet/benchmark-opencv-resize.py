5ef03c706ddb3c3b24669273e672fffdb22792bc
Yuxin Wu <ppwwyyxxc@gmail.com>
2018-07-02 23:43:42 -0700

    update docs about opencv

--- ImageNet/benchmark-opencv-resize.py
+++ ImageNet/benchmark-opencv-resize.py
@@ -8,12 +8,19 @@ import time
 import numpy as np
 
 """
-Some prebuild opencv is much slower than others.
+Some prebuilt opencv is much slower than others.
+You should check with this script and make sure it prints < 1s.
+
 
 On E5-2680v3, archlinux, this script prints:
+    0.61s for system opencv 3.4.0-2.
+    >5 s for anaconda opencv 3.3.1 py36h6cbbc71_1.
 
-0.61s for system opencv 3.4.0-2.
->5 s for anaconda opencv 3.3.1 py36h6cbbc71_1.
+On E5-2650v4, this script prints:
+    0.6s for opencv built locally with -DWITH_OPENMP=OFF
+    0.6s for opencv from `pip install opencv-python`.
+    1.3s for opencv built locally with -DWITH_OPENMP=ON
+    2s for opencv from `conda install`.
 """
 
 
